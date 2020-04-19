10b3f8e6a309a47ade199513e0bb4369d41fedc7
gitneelam <n_dwivedi@hotmail.com>
2020-04-18 20:59:21 -0400

    Adding train-model.py

---
+++ train_model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import pickle
+import sys
+
+#load data
+df = pd.read_csv("..\winequality-white.csv")
+
+#split data
+X = df['fixed_acidity'].values.reshape(-1, 1)
+y = df['quality'].values.reshape(-1, 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#train model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#create pickle file
+filename = 'regress_model'
+pickle.dump(regressor, open(filename, 'wb'))
+
